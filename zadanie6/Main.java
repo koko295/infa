02cc407c57e52d39c7ad86689ae8f7b913606f5f
Колесникова Алина Владимировна ИП-514 <astrolapka@mail.ru>
2025-11-04 19:58:11 +0700

    Добавлен метод getCarByBrendAndYearOperational и тесты

--- zadanie6/Main.java
+++ zadanie6/Main.java
@@ -33,5 +33,37 @@ public class Main {
 
         Car[] toyotas = getCarByBrend(mass, "Toyota");
         System.out.println("Найдено Toyota: " + toyotas.length);
+        Car[] oldToyotas = getCarByBrendAndYearOperational(mass, "Toyota", 3);
+        System.out.println("Toyota старше 3 лет: " + oldToyotas.length);
+
+        Car[] oldHondas = getCarByBrendAndYearOperational(mass, "Honda", 4);
+        System.out.println("Honda старше 4 лет: " + oldHondas.length);
+
+        Car[] oldFords = getCarByBrendAndYearOperational(mass, "Ford", 2);
+        System.out.println("Ford старше 2 лет: " + oldFords.length);
+    }
+    // 16) машины марки brend, которые эксплуатируются более years лет
+    public static Car[] getCarByBrendAndYearOperational(Car[] cars, String brend, int years) {
+        // текущий год лучше брать из Java Time:
+        int cyear = java.time.Year.now().getValue();
+
+        int count = 0;
+        for (int i = 0; i < cars.length; i++) {
+            int carAge = cyear - cars[i].getYear();
+            if (cars[i].getBrand().equals(brend) && carAge > years) {
+                count++;
+            }
+        }
+        Car[] result = new Car[count];
+        int in = 0;
+        for (int i = 0; i < cars.length; i++) {
+            int carAge = cyear - cars[i].getYear();
+            if (cars[i].getBrand().equals(brend) && carAge > years) {
+                result[in] = cars[i];
+                in++;
+            }
+        }
+        return result;
     }
 }
+
